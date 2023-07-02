3a43ec6a938da9ed747d9af73b0b39c31dd4e977
lordisrael <josephisrael206@gmail.com>
2023-07-02 13:24:38 +0100

    added necessary changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,14 @@ const express = require('express')
 const authMiddleware = require('../middleware/authMiddleware')
 const router = express.Router()
 
-const { createUser, login, handleRefreshToken, logout} = require('../controllers/auth')
+const { createUser, login, handleRefreshToken, logout, forgotPassword, updatePassword, resetPassword} = require('../controllers/auth')
 
 router.post('/register', createUser)
 router.get('/refresh', handleRefreshToken)
 router.post('/login', login)
 router.get('/logout', logout)
+router.post('/forgot', forgotPassword)
+router.put('/update-password', authMiddleware, updatePassword)
+router.patch('/reset/:token', resetPassword)
 
 module.exports = router
