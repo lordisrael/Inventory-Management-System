acfa3606a1dad07edbc359ed6a4bc8c8fc5e1ded
lordisrael <josephisrael206@gmail.com>
2023-07-02 13:42:02 +0100

    Add department functions

--- controllers/departCtrl.js
+++ controllers/departCtrl.js
@@ -1,5 +1,12 @@
 const Department = require('../models/department')
 const {} = require('../errors')
+const asyncHandler = require('express-async-handler') 
 const {StatusCodes} = require('http-status-codes')
 
-const createDepartment 
+const createDepartment = asyncHandler(async(req, res) => {
+
+})
+
+module.exports = {
+    createDepartment
+}
