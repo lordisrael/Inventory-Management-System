71e56c5575f1f9ff00cb0e8fa8b29ef070df51e5
lordisrael <josephisrael206@gmail.com>
2023-07-05 12:03:24 +0100

    Addding necessary functions

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const cookieParser = require('cookie-parser')
 const express = require('express')
 const app = express()
 
+const authMiddleware = require('./middleware/authMiddleware')
 const notFoundMiddleware = require('./middleware/not-Found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const authRoute= require('./routes/userRoutes')
 const productRoute = require('./routes/productsRoutes')
+const departmentRoute = require('./routes/departmentRoutes')
+const categoryRoute = require('./routes/categoryRoutes')
+const searchRoute = require('./routes/searchRoutes')
 
 const dbConnect = require('./config/db')
 
@@ -17,6 +21,10 @@ app.use(cookieParser())
 
 app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/products', productRoute)
+app.use('/api/v1/department', departmentRoute)
+app.use('/api/v1/category', categoryRoute)
+app.use('/api/v1/search', authMiddleware, searchRoute)
+
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
