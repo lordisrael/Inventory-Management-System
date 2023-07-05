516d783cd084fd33bb3d24d0e5b02faa9a253dc8
lordisrael <josephisrael206@gmail.com>
2023-07-05 12:00:40 +0100

    Updated Product model

--- models/product.js
+++ models/product.js
@@ -19,23 +19,66 @@ var productSchema = new mongoose.Schema({
     brand:{
         type:String,
     },
-    category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category'
-    },
     department: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Department'
+
+    },
+    // department: {
+    //     type: string,
+    //     required: true,
+    //     enum: ['Appliances', 'Building Materials', 'Electrical', 'Flooring and Rugs', 'Kitchen and bath', 'Lightening', 'Plumbing', 'Windows and Doors,', 'Tools']
+    // },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
     },
     quantity: {
         type: Number,
         required: true,
         default: 0
     },
+    weight: {
+        type: Number,
+    },
     image: {
         type: String,
     }
 });
+
+    // productSchema.pre('save', async function(next){
+    //     const department = this.department
+    //     switch(department) {
+    //         case 'Appliances':
+    //             this.schema.path('category').enum(['Refrigerators', 'Microwaves', 'Air Conditioners and fans', 'Ranges, cooktops, and Ovens'])
+    //             break;
+    //         case 'Building Materials':
+    //             this.schema.path('category').enum(['Lumber', 'Bricks and Blocks', 'Plywood', 'Cements'])
+    //             break;
+    //         case 'Electrical':
+    //             this.schema.path('category').enum(['Wire and Cable', 'Light switchers and Dimmers', 'Bulbs', 'Detectors'])
+    //             break;
+    //         case 'Flooring and rugs':
+    //             this.schema.path('category').enum(['Floor, Wall Tile', 'Vinyl', 'Carpet & Carpet Tiles', 'Area Rugs'])
+    //             break;
+    //         case 'Kitchen and Bath':
+    //             this.schema.path('category').enum(['Cabinets', 'Sinks', 'Faucets', 'Showers'])
+    //             break;
+    //         case 'Lightening':
+    //             this.schema.path('category').enum(['Chandeliers', 'Lamp', 'Outdoor Lights', 'Under cabinet Lightening'])
+    //             break;
+    //         case 'Plumbing':
+    //             this.schema.path('category').enum(['Toilet seat', 'Water Heaters', 'Shower base or pan', 'Tub'])
+    //             break;
+    //         case 'Windows and Doors':
+    //             this.schema.path('category').enum(['Double, single Hung Windows', 'Bay windows', 'Patio Doors', 'Garage Doors'])
+    //             break;
+    //         case 'Tools':
+    //             this.schema.path('category').enum(['Power tools', 'Hand tools', 'Air Compressors', 'Safety Equipment'])
+    //             break;
+
+    //     }
+    // })
 
 //Export the model
 module.exports = mongoose.model('Product', productSchema);
