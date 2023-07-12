b32642e6765bcc170f0cf9955b3c449f4c4edbc6
lordisrael <josephisrael206@gmail.com>
2023-07-13 00:37:56 +0100

    Added another Transaction routes

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -2,8 +2,9 @@ const express = require('express')
 const authMiddleware = require('../middleware/authMiddleware')
 const router = express.Router()
 
-const { getATransaction } = require('../controllers/transactionCtrl')
+const { getATransaction, getTransactionWeek } = require('../controllers/transactionCtrl')
 
 router.get('/get-atransaction/:id', authMiddleware, getATransaction)
+router.get('/week', authMiddleware, getTransactionWeek)
 
 module.exports = router
