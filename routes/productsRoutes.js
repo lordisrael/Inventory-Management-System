2ebc02841c5aaf657254f2da3ad2aa5766a655b1
lordisrael <josephisrael206@gmail.com>
2023-07-12 14:38:12 +0100

    added isSeller middleware

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const authMiddleware = require('../middleware/authMiddleware')
+const isSeller = require('../middleware/isSeller')
 const router = express.Router()
 
 const { createProduct, sellProducts, getAProduct, addquantitytoProduct, deleteProduct } = require('../controllers/productsCtrl')
 const auth = require('../middleware/authMiddleware')
 
 router.post('/create', authMiddleware, createProduct)
-router.post('/sell', authMiddleware, sellProducts)
+router.post('/sell', authMiddleware, isSeller,  sellProducts)
 router.get('/get-aproduct/:id', authMiddleware, getAProduct)
 router.delete('/delete/:id', authMiddleware, deleteProduct)
 router.put('/add/:id', authMiddleware, addquantitytoProduct)
