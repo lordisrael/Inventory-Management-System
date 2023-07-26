862a1c7fd550512933daaa9e4952e1e332039797
lordisrael <josephisrael206@gmail.com>
2023-07-26 10:32:29 +0100

    Added isAdmin middleware

--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const { updateDepartment, deleteDepartment, getDepartment, getAllDepartment, createDepartment } = require('../controllers/departCtrl')
 const authMiddleware = require('../middleware/authMiddleware')
+const isAdmin = require('../middleware/isAdmin')
 
 
-router.put('/edit', authMiddleware, updateDepartment)
-router.post('/create', authMiddleware, createDepartment)
-router.delete('/delete/:id', authMiddleware, deleteDepartment)
-router.get('/get/:id', authMiddleware, getDepartment)
-router.get('/getall', authMiddleware, getAllDepartment)
+router.put('/edit', authMiddleware, isAdmin, updateDepartment)
+router.post('/create', authMiddleware,isAdmin, createDepartment)
+router.delete('/delete/:id', authMiddleware,isAdmin, deleteDepartment)
+router.get('/get/:id', authMiddleware, isAdmin, getDepartment)
+router.get('/getall', authMiddleware, isAdmin, getAllDepartment)
 
 module.exports = router
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const authMiddleware = require('../middleware/authMiddleware')
 const isSeller = require('../middleware/isSeller')
+const isAdmin = require('../middleware/isAdmin')
 const router = express.Router()
 
 const { createProduct, sellProducts, getAProduct, addquantitytoProduct, deleteProduct } = require('../controllers/productsCtrl')
 const auth = require('../middleware/authMiddleware')
 
-router.post('/create', authMiddleware, createProduct)
+router.post('/create', authMiddleware,isAdmin, createProduct)
 router.post('/sell', authMiddleware, isSeller,  sellProducts)
 router.get('/get-aproduct/:id', authMiddleware, getAProduct)
-router.delete('/delete/:id', authMiddleware, deleteProduct)
-router.put('/add/:id', authMiddleware, addquantitytoProduct)
+router.delete('/delete/:id', authMiddleware, isAdmin, deleteProduct)
+router.put('/add/:id', authMiddleware,isAdmin, addquantitytoProduct)
 
 module.exports = router
