908d40b89e534b8dc4fe4608570fbb306c4a9548
lordisrael <josephisrael206@gmail.com>
2023-08-06 08:03:14 +0100

    Added extra security

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const cookieParser = require('cookie-parser')
 const express = require('express')
 const app = express()
 
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const authMiddleware = require('./middleware/authMiddleware')
 const notFoundMiddleware = require('./middleware/not-Found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -17,6 +22,16 @@ const transactionRoutes = require('./routes/transactionRoutes')
 
 const dbConnect = require('./config/db')
 
+app.set('trust proxy', 1)
+app.use(
+    rateLimiter({
+      windowMs: 15 * 60 * 1000,
+      max: 100
+    })
+  )
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 app.use(express.json())
 app.use(cookieParser())
 
